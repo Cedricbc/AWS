1b13491d3ce64059ef2e18097bd0f1c9da48f3bf
kimboy <kimboy@gmail.com>
2021-06-17 11:03:41 +0530

    update

--- app/Models/Movies.php
+++ app/Models/Movies.php
@@ -16,6 +16,9 @@ class Movies extends Model
         if(env('APP_ENV') == 'prod'){
             return env('AWS_BUCKET_URL').'storage/Documents/'.$value;
         }
+        if(env('APP_ENV') == 'cloud'){
+            return env('AWS_CLOUD_FRONT_URL').'storage/Documents/'.$value;
+        }
         return env('APP_URL').'storage/Documents/'.$value;
     }
 }
